87bb8190e2dbd37f4b10eb2be6057f92aec85261
CharvyDhand <charvydhand13@gmail.com>
2025-09-14 01:16:32 +0530

    update

--- 00classwork/MainClass.java
+++ 00classwork/MainClass.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class MainClass{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Chatbot cb=new Chatbot("User", 0);
+        Chatbot cb=new Chatbot();
         HashMap<String, List<String>> FriendsChat=new HashMap<>();
         HashMap<String, List<String>> PersonalChat=new HashMap<>();
-        System.out.println("Enter number of your friends:");
+        System.out.print("Enter number of your friends:");
         int n=sc.nextInt();
         sc.nextLine();
         for(int i=0;i<n;i++){
@@ -18,74 +18,68 @@ public class MainClass{
         System.out.println("2. To delete message for me ");
         System.out.println("3. To delete messege for everyone");
         System.out.println("4. To update a messege");
-        System.out.println("5. Exit the program");
+        System.out.println("5. Display");
+        System.out.println("6. Exit the program");
         int choice;
+        String name;
+        int index;
+        String messege;
         do{
-            System.out.println("Enter your choice");
+            System.out.print("Enter your choice");
             choice=sc.nextInt();
             sc.nextLine();
             switch(choice){
                 case 1:
-                 System.out.println("Enter your friend name:");
-                 String nm=sc.nextLine();
-                 System.out.println("Enter you text:");
-                 String mess=sc.nextLine();
-                 cb.sendMessege(FriendsChat,mess,nm);
-                 cb.sendMessege(PersonalChat, mess,nm);
+                 System.out.print("Enter your friend name:");
+                 name=sc.nextLine();
+                 System.out.print("Enter you text:");
+                 messege=sc.nextLine();
+                 cb.sendMessege(FriendsChat,messege,name);
+                 cb.sendMessege(PersonalChat, messege,name);
                  System.out.println("Messege sent");
                  break;
                 case 2:
-                 System.out.println("Enter friend name:");
-                 String nm1=sc.nextLine();
-                 System.out.println("Enter your text id:");
-                 int mid=sc.nextInt();
+                 System.out.print("Enter friend name:");
+                 name=sc.nextLine();
+                 System.out.print("Enter your text id:");
+                 index=sc.nextInt();
                  sc.nextLine();
-                 cb.delete(FriendsChat,PersonalChat,nm1,mid);
-                 System.out.println("Messege deleted for everyone");
+                 cb.delete(PersonalChat,name,index);
+                 System.out.println("Messege deleted for me");
                  break;
                 case 3:
                  System.out.println("Enter friend name:");
-                 String nm3=sc.nextLine();
+                 name=sc.nextLine();
                  System.out.println("Enter your text id:");
-                 int mid1=sc.nextInt();
-                 sc.nextInt();
-                 cb.delete(PersonalChat,nm3,mid1);
+                 index=sc.nextInt();
+                 sc.nextLine();
+                 cb.delete(FriendsChat, PersonalChat,name,index);
                  System.out.println("Messege deleted for me ");
                  break;
                 case 4:
-                 System.out.println("Enter friend name:");
-                 String nm4=sc.nextLine();
-                 System.out.println("Enter your text id:");
-                 int mid2=sc.nextInt();
-                 sc.nextInt();
-                 System.out.println("Enter new messege:");
-                 String newmess=sc.nextLine();
-                 cb.updateText(PersonalChat,FriendsChat,nm4,mid2,newmess);
+                 System.out.print("Enter friend name:");
+                 name=sc.nextLine();
+                 System.out.print("Enter your text id:");
+                 index=sc.nextInt();
+                 sc.nextLine();
+                 System.out.print("Enter new messege:");
+                 messege=sc.nextLine();
+                 cb.updateText(PersonalChat,FriendsChat,name,index,messege);
                  break;
                 case 5:
+                 System.out.println("The text sent to all:");
+                 cb.display(FriendsChat);
+                 break;
+                case 6:
                  System.out.println("Program exiting");
                  break;
                 default:
                  System.out.println("You entered a default value");
             }
-        }while(choice!=5);
+        }while(choice!=6);
     }    
 }
-class People{
-    public String name;
-    public int uid;
-    public People(String name,int uid){
-        this.name=name;
-        this.uid=uid;
-    }
-    public People(String name){
-        this.name=name;
-    }
-}
-class Chatbot extends People{
-    public Chatbot(String name,int uid){
-        super(name,uid);
-    }
+class Chatbot{
     public void sendMessege(HashMap<String, List<String>> map, String text,String name){
        map.get(name).add(text);
     }
@@ -99,5 +93,10 @@ class Chatbot extends People{
     public void updateText(HashMap<String, List<String>> map1, HashMap<String, List<String>> map2, String nm, int mid, String mess){
         map1.get(nm).set(mid,mess);
         map2.get(nm).set(mid,mess);
+    }
+    public void display(HashMap<String, List<String>> map){
+        for(Map.Entry<String, List<String>> e:map.entrySet()){
+            System.out.println(e.getKey()+":"+e.getValue());
+        }
     }
 }
